af1aae4002030c6ee72569243a340ac36b505b4b
Villalba <lmv@f84d899822fc.ant.amazon.com>
2022-12-05 18:37:22 +0200

    Initial commit

---
+++ test/cdk-dynamodb-global-tables.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkDynamodbGlobalTables from '../lib/cdk-dynamodb-global-tables-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-dynamodb-global-tables-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkDynamodbGlobalTables.CdkDynamodbGlobalTablesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
